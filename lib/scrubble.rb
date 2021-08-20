07d7b8cea99d7d1348b07eb12728961b188c58f6
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-08-20 23:34:13 +0100

    Refactored with introduction of first hash with only two letters and computation of score

--- lib/scrubble.rb
+++ lib/scrubble.rb
@@ -1,9 +1,22 @@
+
 def scrubble(word)
-  if word == "A"
-    1
-  elsif word == "D"
-    2
-  elsif word == "DD"
-    4
-  end
+
+  alphabet = {
+    "A" => 1,
+    "D" => 2
+  }
+
+  score = 0
+
+  word.each_char { |char| score += alphabet[char] }
+  score
+  # if word == "A"
+  #   1
+  # elsif word == "D"
+  #   2
+  # elsif word == "DD"
+  #   4
+  # end
 end
+
+p scrubble("AD")
